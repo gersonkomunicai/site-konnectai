c2d940b061abcfea3438323c32c45a7d923e8ea9
Gerson Junior <gersonjunior2@outlook.com>
2026-09-02 18:46:48 -0300

    ajuste de formulário, primeiros passos

--- components/FormCta.tsx
+++ components/FormCta.tsx
@@ -11,47 +11,61 @@ interface FormCtaProps {
 }
 
 
-export default function FormCta({className}: FormCtaProps) {
+export default function FormCta({ className }: FormCtaProps) {
     const [site, setSite] = useState(false)
 
-    return (
-        <div className={`flex gap-3 flex-col border ${className} rounded-xl p-3`}>
-            <h4>Junte-se a nós e transforme sua comunicação!</h4>
-            <label>Nome*</label>
-            <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
-                <LuUser className='shrink-0 size-4 w-4 h-4' />
-                <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='Como você gostaria de ser chamado?' name="nome" id="" />
-            </div>
-            <label>Empresa*</label>
-            <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
-                <ImOffice className='shrink-0 size-4 w-4 h-4' />
-                <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='Qual o nome da sua empresa?' name="nome" id="" />
-            </div>
-            <label>E-mail*</label>
-            <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
-                <MdOutlineAlternateEmail className='shrink-0 size-4 w-4 h-4' />
-                <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='seumelhor@email.com' name="nome" id="" />
-            </div>
-            <label>Celular*</label>
-            <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
-                <CgSmartphone className='shrink-0 size-4 w-4 h-4' />
-                <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='(12)987654321' name="nome" id="" />
-            </div>
-            <div className={`${site ? 'hidden' : ''}`}>
-                <label>Site da Empresa</label>
-                <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
-                    <GiWorld className='shrink-0 size-4 w-4 h-4' />
-                    <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='https://www.suaempresa.com.br' name="nome" id="" />
-                </div>
-            </div>
-            <div className="flex items-center gap-2">
-                <input type="checkbox" name="checkSite" onClick={() => { setSite(!site) }} />
-                <label >Não tenho site</label>
-            </div>
-            <div className="w-full p-3 bg-paleta-konnectai-secondary font-medium text-paleta-konnectai-primary rounded-full flex justify-center">
-                <span>Solicitar atendimento</span>
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const name = formData.get("name");
+        const enterprise = formData.get("enterprise");
+        const email = formData.get("email");
+        const phone = formData.get("phone");
+        const website = formData.get("website");
 
+        console.log({name, enterprise, email, phone, website})
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={`flex gap-3 flex-col border ${className} rounded-xl p-3`}>
+                <h4>Junte-se a nós e transforme sua comunicação!</h4>
+                <label>Nome*</label>
+                <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
+                    <LuUser className='shrink-0 size-4 w-4 h-4' />
+                    <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='Como você gostaria de ser chamado?' name="name" id="" />
+                </div>
+                <label>Empresa*</label>
+                <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
+                    <ImOffice className='shrink-0 size-4 w-4 h-4' />
+                    <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='Qual o nome da sua empresa?' name="enterprise" id="" />
+                </div>
+                <label>E-mail*</label>
+                <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
+                    <MdOutlineAlternateEmail className='shrink-0 size-4 w-4 h-4' />
+                    <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='seumelhor@email.com' name="email" id="" />
+                </div>
+                <label>Celular*</label>
+                <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
+                    <CgSmartphone className='shrink-0 size-4 w-4 h-4' />
+                    <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='(12)987654321' name="phone" id="" />
+                </div>
+                <div className={`${site ? 'hidden' : ''}`}>
+                    <label>Site da Empresa</label>
+                    <div className='border flex items-center text-paleta-konnectai-terciary p-2 gap-2'>
+                        <GiWorld className='shrink-0 size-4 w-4 h-4' />
+                        <input className='placeholder:text-paleta-konnectai-terciary focus:outline-none w-full' type="text" placeholder='https://www.suaempresa.com.br' name="website" id="" />
+                    </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <input type="checkbox" name="checkSite" onClick={() => { setSite(!site) }} />
+                    <label >Não tenho site</label>
+                </div>
+                <button type="submit" className="w-full p-3 bg-paleta-konnectai-secondary font-medium text-paleta-konnectai-primary rounded-full flex justify-center">
+                    <span>Solicitar atendimento</span>
+
+                </button>
             </div>
-        </div>
+        </form>
     )
 }
